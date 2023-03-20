cc816932bdb382cc40ff0feb8c86a56cad359b78
Bakhromjon <xbakhromjon@gmail.com>
2023-03-20 21:46:07 +0500

    init

---
+++ src/main/java/uz/bakhromjon/Main.java
@@ -0,0 +1,17 @@
+package uz.bakhromjon;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException, InterruptedException {
+        File file = new File("C:\\Users\\Bakhromjon\\IdeaProjects\\asci-art\\src\\main\\resources\\myphotoorg.ansi.txt");
+        Scanner reader = new Scanner(file);
+        while (reader.hasNextLine()) {
+            String line = reader.nextLine();
+            System.out.println(line);
+            Thread.sleep(50);
+        }
+    }
+}
